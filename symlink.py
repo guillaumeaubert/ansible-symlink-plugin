80ba15d1e2189a21a6bd84a25e80ae63110aa3b3
Guillaume Aubert <aubertg@cpan.org>
2016-04-16 10:12:13 -0700

    Relicense with the MIT License.

--- symlink.py
+++ symlink.py
@@ -2,19 +2,10 @@
 
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014-2015 Guillaume Aubert.
+# Copyright 2014-2016 Guillaume Aubert.
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License version 3 as published by the
-# Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# This software is released under the MIT license. See the LICENSE file for
 # details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program. If not, see http://www.gnu.org/licenses/
 
 import shutil
 
